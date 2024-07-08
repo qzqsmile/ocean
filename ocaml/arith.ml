905223361c6034143a39072b59c96ee2da4ef6f5
qzqsmile <qzqsmile@gmail.com>
2024-07-08 20:43:15 +0800

    add-ocaml-arith-solutions

--- ocaml/arith.ml
+++ ocaml/arith.ml
@@ -1,16 +1,90 @@
+(* Determine Whether a Given Integer Number Is Prime Intermediate *)
 let is_prime n =
   let n = abs n in
   let rec is_not_divisor d =
     d * d > n || (n mod d <> 0 && is_not_divisor (d + 1)) in
-  n <> 1 && is_not_divisor 2;;
+  n > 1 && is_not_divisor 2;;
 
 
+(* Determine the Greatest Common Divisor of Two Positive Integer Numbers Intermediate *)
   let rec gcd a b =
       if b = 0 then a else gcd b (a mod b);;
   gcd 13 27;;
   
   let coprime a b = gcd a b = 1;;
   
-  coprime 13 27;;
+(* Calculate Euler's Totient Function Φ(m) Intermediate *)
+(* coprime 13 27;; *)
+let phi num = 
+    let rec aux c count =
+      if num = 1 then 1
+      else if num = c then count 
+      else if (coprime num  c) then aux (c+1) (count+1)
+      else aux (c+1) count in 
+      aux 1 0;;
 
+
+(* Determine the Prime Factors of a Given Positive Integer *)
+(* factors 315;; *)
+let factors1 num = 
+  let rec aux c acc num = 
+    if 1 = num then [1]
+    else if c = num then num::acc 
+    else if num mod c <> 0 then aux (c+1) acc num
+    else aux 2 (c::acc) (num/c)
+  in List.rev (aux 2 [] num);;
+
+let factors num = 
+  let rec aux cur count acc = function 
+  | [] -> (cur,count)::acc 
+  | h::t -> 
+    if h = cur then aux cur (count+1) acc t 
+    else if count <> 0 then aux h 1 ((cur, count)::acc) t 
+    else aux h 1 acc t 
+  in List.rev (aux 0 0 [] (factors1 num));;
+
+
+  (* Calculate Euler's Totient Function Φ(m) (Improved)？? *)
+
+
+  (* Compare the Two Methods of Calculating Euler's Totient Function *)
+
+
+
+  (* A List of Prime Numbers *)
+  (* List.length (all_primes 2 7920);; *)
+let all_primes b e = 
+  let rec aux cur acc =
+    if cur > e then acc 
+    else if is_prime cur then aux (cur+1) (cur::acc)
+    else aux (cur+1) acc
+  in List.rev (aux b []);;
+
+  (* Goldbach's Conjecture *)
+
+  (* goldbach 28;; *)
+
+let goldbach num = 
+  let rec aux c1 c2 = 
+    if is_prime c1 && is_prime c2 then (c1, c2)
+    else aux (c1+1) (c2-1) in 
+    aux 1 (num-1);;
+
+
+
+    (* A List of Goldbach Compositions *)
+
+    (* goldbach_list 9 20;; *)
+
+  let goldbach_list b e = 
+    let rec aux cur acc = 
+      if cur > e then acc else 
+      if cur mod 2 = 0 then aux (cur+2) ((cur, goldbach(cur))::acc)
+      else aux (cur+1) acc in 
+      List.rev (aux b []);;
+      
   
+
+  (* type 'b rle =
+  | One of 'b
+  | Many of int * 'b;; *)
--- ocaml/list.ml
+++ ocaml/list.ml
@@ -62,6 +62,38 @@ let is_palindrome list =
 is_palindrome [1;2;3;2;1]
 
 
+(* Flatten a list *)
+(* flatten [One "a"; Many [One "b"; Many [One "c" ;One "d"]; One "e"]];; *)
+(* type 'a node =
+  | One of 'a node
+  | Many of 'a node list *)
+
+(* let flatten list =
+    let rec aux acc = function
+      | [] -> acc
+      | One x :: t -> aux (x :: acc) t
+      | Many l :: t -> aux (aux acc l) t
+    in
+    List.rev (aux [] list);; *)
+
+
+
+(* # compress ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];; *)
+let compress list = 
+  let rec aux acc = function 
+  | [] -> acc
+  | [x] -> x :: acc 
+  | a :: (b :: _ as t) ->
+    if a = b then aux acc t
+    else aux (a::acc) t in 
+  List.rev (aux [] list);;
+
+
+
+
+
+
+
 let pack list =
   let rec aux current acc = function
     | [] -> []    (* Can only be reached if original list is empty *)
@@ -71,32 +103,205 @@ let pack list =
        else aux [] ((a :: current) :: acc) t  in
   List.rev (aux [] [] list);;
 
+(* let  pack  current acc = function 
+  | [] -> []    (* Can only be reached if original list is empty *)
+  | [x] -> (x :: current) :: acc
+  | a :: (b :: _ as t) ->
+    if a = b then pack (a :: current) acc t
+    else pack [] ((a :: current) :: acc) t;; *)
+
+
+(* encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];; *)
+(* [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")] *)
+(* let encode list  = 
+    let rec aux current acc = function 
+    | [] -> []
+    | [x] -> (current, x) :: acc
+    | h1 :: (h2 :: t) ->
+      if h1 = h2 then aux (current+1) acc (h2::t)
+      else aux 1 ((current+1, h1)::acc) t;; *)
+
+
+(* pack ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e"];; *)
+(* [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+ ["e"; "e"; "e"; "e"]] *)
+
+
 
   (* Run-length encoding of a list. *)
-  let encode list =
+  (* encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];; *)
+  (* let encode list =
     let rec aux count acc = function
       | [] -> [] (* Can only be reached if original list is empty *)
       | [x] -> (count + 1, x) :: acc
       | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
                               else aux 0 ((count + 1, a) :: acc) t in
-    List.rev (aux 0 [] list);;
+    List.rev (aux 0 [] list);;  *)
+
+
+(* Modified Run-Length Encodin *)
+(* encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];; *)
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a 
+(* 
+let encode list =
+    let create count v = 
+      if count = 1 then One v else Many(count, v)
+    in
+    let rec aux count acc = function
+      | [] -> [] (* Can only be reached if original list is empty *)
+      | [x] -> (create (count+1) x) :: acc
+      | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+                              else aux 0 (create (count+1) a :: acc) t in
+    List.rev (aux 0 [] list);; *)
+
+(* decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")];; *)
+
+(* let decode list = 
+  let rec many count v acc =
+    match count with 
+    | 0 -> acc 
+    | _ -> many (count-1) v (v::acc) 
+  in 
+  let rec aux acc = function 
+  | []->acc
+  | One h :: t -> aux (h::acc) t
+  (* | Many (n, x) :: t -> aux (many acc n x) t *)
+  | Many (n, x) :: t -> aux ((many n x [])@acc) t
+  (* | Many (n, x) :: t -> aux (many n x [])@acc t *)
+in List.rev (aux [] list) *)
+
+(* Run-Length Encoding of a List (Direct Solution) *)
+(* todo *)
+
 
 
 (* Duplicate the elements of a list. (easy) *)
-let duplicate = function
+(* let duplicate = function
       | [] -> []
       | [h :: t] -> h :: h :: duplicate t;;
 
-duplicate ["a"; "b"; "c"; "c"; "d"];;
+duplicate ["a"; "b"; "c"; "c"; "d"];; *)
 
 (* . Replicate the elements of a list a given number of times. *)
 
-let duplciate count = function
+(* let duplciate count = function
      | [] -> []
-     | h :: t -> if count == 1 then h :: duplciate count t
-                 else 
+     | h :: t -> if count == 1 then h :: duplciate count t *)
 
-(* Remove the K'th element from a list. (easy) *)
-rec remove_at n = function
+(* Replicate the Elements of a List a Given Number of Times *)
+(* replicate ["a"; "b"; "c"] 3;; *)
+(* let replicate list count= 
+  let rec duplicate count v acc = 
+    if count == 0 then acc 
+    else duplicate (count-1) v (v::acc)
+  in 
+  let rec aux count acc = function
   | [] -> []
-  | h :: t -> if n == 0 then t else h :: remove_at (n-1) t;;
+  | [x] -> duplicate count x acc
+  | h::t -> aux count (duplicate count h acc) t
+in List.rev (aux count [] list) *)
+
+(* Drop Every N'th Element From a List *)
+(* drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;; *)
+(* let drop list count = 
+  let rec aux  cur acc = function 
+  | []->acc 
+  | h::t-> if cur = 1 then aux  count acc t 
+  else aux (cur-1) (h::acc) t 
+in List.rev (aux count [] list) *)
+  
+
+(* let split list n =
+  let rec aux i acc = function
+    | [] -> List.rev acc, []
+    | h :: t as l -> if i = 0 then List.rev acc, l
+                     else aux (i - 1) (h :: acc) t 
+  in
+    aux n [] list;; *)
+
+(* Extract a Slice From a List Interme *)
+(* slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 6;; *)
+(* let slice s a b = 
+  let rec aux count acc = function 
+  | [] -> []
+  | h :: t -> if count <= a then aux (count+1) acc t
+  else if count > b then acc 
+  else aux (count+1) (h::acc) t in 
+  List.rev (aux 1 [] s);; *)
+
+(* Rotate a List N Places to the Left Intermediate *)
+(* rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3;; *)
+(* let rotate list count = 
+  let rec aux cur acc = function
+  | []->[]
+  | h::t as l -> 
+    if cur = 0 then (l @ (List.rev acc))
+    else aux (cur-1) (h::acc) t in 
+    aux count [] list;; *)
+
+
+(* Remove the K'th Element From a List Beginner *)
+
+(* remove_at 1 ["a"; "b"; "c"; "d"];; *)
+
+(* let remove_at count list = 
+  let rec aux cur acc = function 
+  | []->[]
+  | h::t  -> 
+    if cur = 1 then (List.rev acc) @ t 
+    else aux (cur-1) (h::acc) t in 
+    aux count [] list  *)
+
+(* Insert an Element at a Given Position Into a List Beginner *)
+(* insert_at "alfa" 1 ["a"; "b"; "c"; "d"];; *)
+(* let rec insert_at str count list = 
+  let rec aux cur acc = function 
+  | []->[str]
+  | h::t as l -> 
+    if cur = 0 then (List.rev acc)  @ (str::l)
+    else aux (cur-1) (h::acc) t 
+  in aux count [] list  *)
+
+
+  (* Create a List Containing All Integers Within a Given Range *)
+
+(* let range b e = 
+  let rec aux cur acc = 
+    if cur >= b then if cur <= e then aux (cur+1) (cur::acc) else acc else []
+    in List.rev (aux b []);; *)
+
+
+(* Extract a Given Number of Randomly Selected Elements From a List Intermediate *)
+(* rand_select ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3;; *)
+let rand_select list n = 
+  Random.init 0;
+  let rec extract k acc= function 
+  | [] -> []
+  | h::t  -> 
+    if k  = 0 then (List.rev acc) @ t 
+    else extract (k-1) (h::acc) t
+  in 
+  let rec extract_n k acc = 
+    if k > 0 then extract_n (k-1) (extract (Random.int (List.length acc)) [] acc)
+    else acc 
+  in extract_n (List.length list - n) list;;
+
+
+  (* Lotto: Draw N Different Random Numbers From the Set 1..M *)
+  (* lotto_select 6 49;; *)
+(* 
+  let lotto_select n m = 
+  let rec aux acc =  *)
+
+(* let lotto_select n m = rand_select (range 1 m) n;; *)
+(* Generate a Random Permutation of the Elements of a List *)
+(* let permutation ["a"; "b"; "c"; "d"; "e"; "f"];; *)
+
+let permutation list = rand_select list (List.length list);;
+permutation ["a"; "b"; "c"; "d"; "e"; "f"]
+(* let permutation list = rand_select list (List.length list) *)
+
+
+(* Generate the Combinations of K Distinct Objects Chosen From the N Elements of a List *)
